8d471b5b7a8f57214afe79783f09afa97c5d2bfc
Jesse Kempf <jessekempf@gmail.com>
2010-02-28 00:23:02 -0500

    Add absolute and relative entropy functions.

--- entropy/__init__.py
+++ entropy/__init__.py
@@ -7,5 +7,26 @@ def entropy(data):
 	there are per octet in the string."""
 	return _entropy.shannon_entropy(data)
 
+def absolute_entropy(data):
+	"""Compute the "absolute" entropy of the given string.
+
+	The absolute entropy of a string is how many bits of information,
+	total, are in the entire string. This is the same as the Shannon entropy
+	multiplied by the length of the string.
+
+	A string can be losslessly compressed to a size no smaller than its
+	absolute entropy."""
+	
+	return entropy(data) * len(data)
+
+def relative_entropy(data):
+	"""Compute the relative entropy of the given string.
+
+	The relative entropy is the ratio of the entropy of a string to its size,
+	i.e., a measure of how well it uses space. It is, therefore, a floating
+	point value on the interval (0, 1]."""
+
+	return entropy(data) / 8
+
 if __name__ == '__main__':
 	print entropy('\n'.join(file(__file__)))
